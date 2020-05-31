43fa35948e0f79a29ce3024fed4bab2fd5e4ea6e
Ben Kulcsar <b.l.kulcsar@gmail.com>
2020-05-31 10:06:41 +0100

    request bugfix

--- scraper_tools/title_list_scraper.py
+++ scraper_tools/title_list_scraper.py
@@ -4,8 +4,8 @@ from collections import OrderedDict
 
 
 def get_content(url):
-    headers = {'User-Agent': '''Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5)
-                                AppleWebKit/537.36 (KHTML, like Gecko)
+    headers = {'User-Agent': '''Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) \
+                                AppleWebKit/537.36 (KHTML, like Gecko) \
                                 Chrome/50.0.2661.102 Safari/537.36'''}
     page = requests.get(url, headers=headers)
     return BeautifulSoup(page.content, 'html.parser')
