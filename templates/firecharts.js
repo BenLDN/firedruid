6fb1b25e312808d8a96498c719a1786cf3bd8d48
Ben Kulcsar <b.l.kulcsar@gmail.com>
2020-06-21 13:11:09 +0100

    Fronted code cleanup

--- templates/firecharts.js
+++ templates/firecharts.js
@@ -1,237 +1,243 @@
+// HOURLY LINE GRAPH ===============================================================
 
-                              Chart.defaults.global.responsive = true;
+Chart.defaults.global.responsive = true;
 
-                              var hourlyTrendsChartData = {
-                                labels : [{% for dim in trend_time_dim_hourly %}
-                                           "{{dim}}",
-                                          {% endfor %}],
-                                datasets : [
-                                    {% for word, values, colour in trend_hourly_zip %}
-                                    {
-                                    label: '{{ word }}',
-                                    fill: false,
-                                    lineTension: 0.3,
-                                    borderColor: "{{colour}}",
-                                    pointHoverRadius: 5,
-                                    pointHoverBackgroundColor: "{{colour}}",
-                                    pointHoverBorderColor: "{{colour}}",
-                                    pointRadius: 1,
-                                    pointHitRadius: 15,
-                                    data : {{values}},
+var hourlyTrendsChartData = {
 
-                                    },
-                                    {% endfor %}
+    labels: [{%for dim in trend_time_dim_hourly %} "{{dim}}", {% endfor %}],
 
-                              ]
-                              }
+    datasets: [{% for word, values, colour in trend_hourly_zip %} 
+          {
+            label: '{{ word }}',
+            fill: false,
+            lineTension: 0.3,
+            borderColor: "{{colour}}",
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "{{colour}}",
+            pointHoverBorderColor: "{{colour}}",
+            pointRadius: 1,
+            pointHitRadius: 15,
+            data: {{values}},
+          },
+        {% endfor %}
 
-                              var ctx = document.getElementById("hourlyTrendsChart").getContext("2d");
+    ]
+};
 
-                              var hourlyTrendsChart = new Chart(ctx, {
-                                type: 'line',
-                                data: hourlyTrendsChartData,
-                                options: {maintainAspectRatio: false,
+var ctx = document.getElementById("hourlyTrendsChart").getContext("2d");
 
-                                title: {
-                                    display: true,
-                                    text: 'Word frequency in the last 7 days (top 5 words, hourly)'},
+var hourlyTrendsChart = new Chart(ctx, {
+    type: 'line',
+    data: hourlyTrendsChartData,
+    options: {
+        maintainAspectRatio: false,
 
-                                scales: {
-                                    
-                                    yAxes: [{
-                                    ticks: {
-                                    
-                                           min: 0,
-                                           callback: function(value){return value.toFixed(2)+ "%"}
-                                        },  
-                                        scaleLabel: {
-                                           display: true,
-                                           labelString: "Word Frequency"
-                                        }
-                                    }]
-                                }
-                            }
+        title: {
+            display: true,
+            text: 'Word frequency in the last 7 days (top 5 words, hourly)'
+        },
 
-                              });
+        scales: {
 
-                            Chart.defaults.global.responsive = true;
+            yAxes: [{
+                ticks: {
 
-                            var hourlyTopChartData = 
-                                {
+                    min: 0,
+                    callback: function(value) {
+                        return value.toFixed(2) + "%"
+                    }
+                },
+                scaleLabel: {
+                    display: true,
+                    labelString: "Word Frequency"
+                }
+            }]
+        }
+    }
 
-                                labels : [{% for label in top_labels_hourly %}
-                                           "{{label}}",
-                                          {% endfor %}],
-                                datasets : [{
-                                    backgroundColor: "#dc3545",
-                                    data : [{% for val in top_values_hourly %}
-                                              {{val}},
-                                            {% endfor %}]
+});
 
-                                           }]
-                                }
+// HOURLY BAR CHART ===============================================================
 
-                              var ctx = document.getElementById("hourlyTopChart").getContext("2d");
+var hourlyTopChartData = {
 
-                              var hourlyTopChart = new Chart(ctx, {
-                                type: 'horizontalBar',
-                                data: hourlyTopChartData,
+    labels: [{% for label in top_labels_hourly %} "{{label}}", {% endfor %}],
 
-                                options: {maintainAspectRatio: false,
+    datasets: [{
+        backgroundColor: "#dc3545",
+        data: [{% for val in top_values_hourly %} {{val}}, {% endfor %}]
 
-                                legend: {display: false},
+    }]
+};
 
-                                title: {
-                                    display: true,
-                                    text: 'Word frequency in the last 7 days (top 20 words, total)'},
+var ctx = document.getElementById("hourlyTopChart").getContext("2d");
 
-                                    tooltips: {
-                                        custom: function(tooltip) {
-                                            if (!tooltip) return;
-                                            // disable displaying the color box;
-                                            tooltip.displayColors = false;
-                                          },
+var hourlyTopChart = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: hourlyTopChartData,
 
-                                      callbacks: {
-                                        label: function(tooltipItem, data) {
-                                          return data['datasets'][0]['data'][tooltipItem['index']] + '%';
-                                                        }
-                                                      }
-                                                    },
-                                scales: {
-                                    
-                                    xAxes: [{
-                                    ticks: {
-                                    
-                                           min: 0,
-                                           callback: function(value){return value.toFixed(2)+ "%"}
-                                        },  
-                                        scaleLabel: {
-                                           display: false,
-                                        }
-                                    }]
-                                }
-                            }
+    options: {
+        maintainAspectRatio: false,
 
-                              });
+        legend: {
+            display: false
+        },
+
+        title: {
+            display: true,
+            text: 'Word frequency in the last 7 days (top 20 words, total)'
+        },
+
+        tooltips: {
+            custom: function(tooltip) {
+                if (!tooltip) return;
+                // disable displaying the color box;
+                tooltip.displayColors = false;
+            },
+
+            callbacks: {
+                label: function(tooltipItem, data) {
+                    return data['datasets'][0]['data'][tooltipItem['index']] + '%';
+                }
+            }
+        },
+        scales: {
+
+            xAxes: [{
+                ticks: {
+
+                    min: 0,
+                    callback: function(value) {
+                        return value.toFixed(2) + "%"
+                    }
+                },
+                scaleLabel: {
+                    display: false,
+                }
+            }]
+        }
+    }
+
+});
+
+// DAILY LINE GRAPH ================================================================
+
+var dailyTrendsChartData = {
+
+    labels: [{% for dim in trend_time_dim_daily %} "{{dim}}", {% endfor %}],
+
+    datasets: [{% for word, values, colour in trend_daily_zip %} 
+          {
+            label: '{{ word }}',
+            fill: false,
+            lineTension: 0.3,
+            borderColor: "{{colour}}",
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "{{colour}}",
+            pointHoverBorderColor: "{{colour}}",
+            pointRadius: 1,
+            pointHitRadius: 15,
+            data: {{values}},
+
+          },
+        {% endfor %}
+
+    ]
+};
+
+var ctx = document.getElementById("dailyTrendsChart").getContext("2d");
+
+var dailyTrendsChart = new Chart(ctx, {
+    type: 'line',
+    data: dailyTrendsChartData,
+    options: {
+        maintainAspectRatio: false,
+
+        title: {
+            display: true,
+            text: 'Word frequency in the last 30 days (top 5 words, hourly)'
+        },
+
+        scales: {
+
+            yAxes: [{
+                ticks: {
+
+                    min: 0,
+                    callback: function(value) {
+                        return value.toFixed(2) + "%"
+                    }
+                },
+                scaleLabel: {
+                    display: true,
+                    labelString: "Word Frequency"
+                }
+            }]
+        }
+    }
+
+});
+
+// HOURLY BAR CHART ===============================================================
 
 
+var dailyTopChartData = {
 
+    labels: [{% for label in top_labels_daily %} "{{label}}", {% endfor %}],
 
+    datasets: [{
+        backgroundColor: "#dc3545",
+        data: [{% for val in top_values_daily %} {{val}}, {% endfor %}]
 
+    }]
+};
 
-                              Chart.defaults.global.responsive = true;
+var ctx = document.getElementById("dailyTopChart").getContext("2d");
 
-                              var dailyTrendsChartData = {
-                                labels : [{% for dim in trend_time_dim_daily %}
-                                           "{{dim}}",
-                                          {% endfor %}],
-                                datasets : [
-                                    {% for word, values, colour in trend_daily_zip %}
-                                    {
-                                    label: '{{ word }}',
-                                    fill: false,
-                                    lineTension: 0.3,
-                                    borderColor: "{{colour}}",
-                                    pointHoverRadius: 5,
-                                    pointHoverBackgroundColor: "{{colour}}",
-                                    pointHoverBorderColor: "{{colour}}",
-                                    pointRadius: 1,
-                                    pointHitRadius: 15,
-                                    data : {{values}},
+var dailyTopChart = new Chart(ctx, {
+    type: 'horizontalBar',
+    data: dailyTopChartData,
 
-                                    },
-                                    {% endfor %}
+    options: {
+        maintainAspectRatio: false,
 
-                              ]
-                              }
+        legend: {
+            display: false
+        },
 
-                              var ctx = document.getElementById("dailyTrendsChart").getContext("2d");
+        title: {
+            display: true,
+            text: 'Word frequency in the last 30 days (top 20 words, total)'
+        },
 
-                              var dailyTrendsChart = new Chart(ctx, {
-                                type: 'line',
-                                data: dailyTrendsChartData,
-                                options: {maintainAspectRatio: false,
+        tooltips: {
+            custom: function(tooltip) {
+                if (!tooltip) return;
+                // disable displaying the color box;
+                tooltip.displayColors = false;
+            },
 
-                                title: {
-                                    display: true,
-                                    text: 'Word frequency in the last 30 days (top 5 words, hourly)'},
+            callbacks: {
+                label: function(tooltipItem, data) {
+                    return data['datasets'][0]['data'][tooltipItem['index']] + '%';
+                }
+            }
+        },
+        scales: {
 
-                                scales: {
-                                    
-                                    yAxes: [{
-                                    ticks: {
-                                    
-                                           min: 0,
-                                           callback: function(value){return value.toFixed(2)+ "%"}
-                                        },  
-                                        scaleLabel: {
-                                           display: true,
-                                           labelString: "Word Frequency"
-                                        }
-                                    }]
-                                }
-                            }
+            xAxes: [{
+                ticks: {
 
-                              });
+                    min: 0,
+                    callback: function(value) {
+                        return value.toFixed(2) + "%"
+                    }
+                },
+                scaleLabel: {
+                    display: false,
+                }
+            }]
+        }
+    }
 
-                            Chart.defaults.global.responsive = true;
-
-                            var dailyTopChartData = 
-                                {
-
-                                labels : [{% for label in top_labels_daily %}
-                                           "{{label}}",
-                                          {% endfor %}],
-                                datasets : [{
-                                    backgroundColor: "#dc3545",
-                                    data : [{% for val in top_values_daily %}
-                                              {{val}},
-                                            {% endfor %}]
-
-                                           }]
-                                }
-
-                              var ctx = document.getElementById("dailyTopChart").getContext("2d");
-
-                              var dailyTopChart = new Chart(ctx, {
-                                type: 'horizontalBar',
-                                data: dailyTopChartData,
-
-                                options: {maintainAspectRatio: false,
-
-                                legend: {display: false},
-
-                                title: {
-                                    display: true,
-                                    text: 'Word frequency in the last 30 days (top 20 words, total)'},
-
-                                    tooltips: {
-                                        custom: function(tooltip) {
-                                            if (!tooltip) return;
-                                            // disable displaying the color box;
-                                            tooltip.displayColors = false;
-                                          },
-
-                                      callbacks: {
-                                        label: function(tooltipItem, data) {
-                                          return data['datasets'][0]['data'][tooltipItem['index']] + '%';
-                                                        }
-                                                      }
-                                                    },
-                                scales: {
-                                    
-                                    xAxes: [{
-                                    ticks: {
-                                    
-                                           min: 0,
-                                           callback: function(value){return value.toFixed(2)+ "%"}
-                                        },  
-                                        scaleLabel: {
-                                           display: false,
-                                        }
-                                    }]
-                                }
-                            }
-
-                              });
+});
