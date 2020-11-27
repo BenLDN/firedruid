f03f5399c9ac9830072f6a6bcda264cb403b942f
Ben Kulcsar <b.l.kulcsar@gmail.com>
2020-11-27 15:35:24 +0000

    frontend cleanup done

--- templates/firecharts.js
+++ templates/firecharts.js
@@ -1,5 +1,3 @@
-
-var app_load_data = {{app_load_data}}
 var all_data = {{all_data}}
 var colourlist = {{colourlist}}
 var start_dt = "{{default_start_date}}"
@@ -147,6 +145,7 @@ function generate_data(start_dt, end_dt, top_n) {
 
     start_selector = document.getElementById("choose-start")
     end_selector = document.getElementById("choose-end")
+    top_selector = document.getElementById("choose-top-n")
 
     removeOptions(start_selector)
     removeOptions(end_selector)
@@ -172,35 +171,11 @@ function generate_data(start_dt, end_dt, top_n) {
 
     start_selector.value = start_dt
     end_selector.value = end_dt
+    top_selector.value = top_n
 
 }
 
 generate_data(start_dt, end_dt, top_n)
-
-// var slider = document.getElementById('slider');
-//
-// noUiSlider.create(slider, {
-//     start: [start_index, end_index],
-//     connect: true,
-//     margin: 72,
-//     range: {
-//         'min': 0,
-//         'max': all_data['datetimes'].length
-//     }
-// })
-//
-// slider.noUiSlider.on('change', function (values, handle) {
-//
-//   start_index = parseInt(values[0])
-//   end_index = parseInt(values[1])
-//   start_dt = all_data['datetimes'][start_index]
-//   end_dt = all_data['datetimes'][end_index]
-//   console.log(start_dt)
-//   console.log(end_dt)
-//   //start_index = parseInt
-// })
-
-//==
 
 Chart.defaults.global.responsive = true;
 
@@ -326,287 +301,12 @@ var interactiveBarChart = new Chart(ctx, {
 
 });
 
-// OLD CHARTS FROM HERE ===============================================================
-
-// HOURLY LINE GRAPH ===============================================================
-
-var hourlyTrendsChartData = {
-
-    labels: app_load_data['hourly']['Last 7 Days']['trend_data_hourly']['time_dim'],
-
-    datasets: []
-};
-
-for (i = 0; i < 5; i++) {
-
-    dataset = {
-        label: app_load_data['hourly']['Last 7 Days']['trend_data_hourly']['words'][i],
-        fill: false,
-        lineTension: 0.3,
-        borderColor: colourlist[i],
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: colourlist[i],
-        pointHoverBorderColor: colourlist[i],
-        pointRadius: 1,
-        pointHitRadius: 15,
-        data: app_load_data['hourly']['Last 7 Days']['trend_data_hourly']['value_list'][i]
-    }
-
-    hourlyTrendsChartData.datasets.push(dataset)
-}
-
-var ctx = document.getElementById("hourlyTrendsChart").getContext("2d");
-
-var hourlyTrendsChart = new Chart(ctx, {
-    type: 'line',
-    data: hourlyTrendsChartData,
-    options: {
-        maintainAspectRatio: false,
-
-        title: {
-            display: true,
-            text: 'Word frequency in the last 7 days (top 5 words, hourly)'
-        },
-
-        scales: {
-
-            yAxes: [{
-                ticks: {
-
-                    min: 0,
-                    callback: function(value) {
-                        return value.toFixed(2) + "%"
-                    }
-                },
-                scaleLabel: {
-                    display: false,
-                }
-            }]
-        }
-    }
-
-});
-
-// HOURLY BAR CHART ===============================================================
-
-var hourlyTopChartData = {
-
-    labels: app_load_data['hourly']['Last 7 Days']['top_words_hourly']['labels'],
-
-    datasets: [{
-        backgroundColor: "#dc3545",
-        data: app_load_data['hourly']['Last 7 Days']['top_words_hourly']['values']
-
-    }]
-};
-
-var ctx = document.getElementById("hourlyTopChart").getContext("2d");
-
-var hourlyTopChart = new Chart(ctx, {
-    type: 'horizontalBar',
-    data: hourlyTopChartData,
-
-    options: {
-        maintainAspectRatio: false,
-
-        legend: {
-            display: false
-        },
-
-        title: {
-            display: true,
-            text: 'Word frequency in the last 7 days (top 20 words, total)'
-        },
-
-        tooltips: {
-            custom: function(tooltip) {
-                if (!tooltip) return;
-                // disable displaying the color box;
-                tooltip.displayColors = false;
-            },
-
-            callbacks: {
-                label: function(tooltipItem, data) {
-                    return data['datasets'][0]['data'][tooltipItem['index']] + '%';
-                }
-            }
-        },
-        scales: {
-
-            xAxes: [{
-                ticks: {
-
-                    min: 0,
-                    callback: function(value) {
-                        return value.toFixed(2) + "%"
-                    }
-                },
-                scaleLabel: {
-                    display: false,
-                }
-            }]
-        }
-    }
-
-});
-
-// DAILY LINE GRAPH ================================================================
-
-var dailyTrendsChartData = {
-
-    labels: app_load_data['daily']['latest']['trend_data_daily']['time_dim'],
-
-    datasets: []
-
-};
-
-for (i = 0; i < 5; i++) {
-
-    dataset = {
-        label: app_load_data['daily']['latest']['trend_data_daily']['words'][i],
-        fill: false,
-        lineTension: 0.3,
-        borderColor: colourlist[i],
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: colourlist[i],
-        pointHoverBorderColor: colourlist[i],
-        pointRadius: 1,
-        pointHitRadius: 15,
-        data: app_load_data['daily']['latest']['trend_data_daily']['value_list'][i]
-    }
-
-    dailyTrendsChartData.datasets.push(dataset)
-}
-
-var ctx = document.getElementById("dailyTrendsChart").getContext("2d");
-
-var dailyTrendsChart = new Chart(ctx, {
-    type: 'line',
-    data: dailyTrendsChartData,
-    options: {
-        maintainAspectRatio: false,
-
-        title: {
-            display: true,
-            text: 'Word frequency in the last 30 days (top 5 words, hourly)'
-        },
-
-        scales: {
-
-            yAxes: [{
-                ticks: {
-
-                    min: 0,
-                    callback: function(value) {
-                        return value.toFixed(2) + "%"
-                    }
-                },
-                scaleLabel: {
-                    display: false,
-                }
-            }]
-        }
-    }
-
-});
-
-// DAILY BAR CHART ===============================================================
-
-
-var dailyTopChartData = {
-
-    labels: app_load_data['daily']['latest']['top_words_daily']['labels'],
-
-    datasets: [{
-        backgroundColor: "#dc3545",
-        data: app_load_data['daily']['latest']['top_words_daily']['values']
-
-    }]
-};
-
-var ctx = document.getElementById("dailyTopChart").getContext("2d");
-
-var dailyTopChart = new Chart(ctx, {
-    type: 'horizontalBar',
-    data: dailyTopChartData,
-
-    options: {
-        maintainAspectRatio: false,
-
-        legend: {
-            display: false
-        },
-
-        title: {
-            display: true,
-            text: 'Word frequency in the last 30 days (top 20 words, total)'
-        },
-
-        tooltips: {
-            custom: function(tooltip) {
-                if (!tooltip) return;
-                // disable displaying the color box;
-                tooltip.displayColors = false;
-            },
-
-            callbacks: {
-                label: function(tooltipItem, data) {
-                    return data['datasets'][0]['data'][tooltipItem['index']] + '%';
-                }
-            }
-        },
-        scales: {
-
-            xAxes: [{
-                ticks: {
-
-                    min: 0,
-                    callback: function(value) {
-                        return value.toFixed(2) + "%"
-                    }
-                },
-                scaleLabel: {
-                    display: false,
-                }
-            }]
-        }
-    }
-
-});
-
-
-function change_week(wk) {
-
-    hourlyTopChart.data.labels = app_load_data['hourly'][wk]['top_words_hourly']['labels']
-    hourlyTopChart.data.datasets[0].data = app_load_data['hourly'][wk]['top_words_hourly']['values']
-    hourlyTopChart.update()
-
-    hourlyTrendsChart.data.labels = app_load_data['hourly'][wk]['trend_data_hourly']['time_dim']
-    hourlyTrendsChartData.datasets = []
-    for (i = 0; i < 5; i++) {
-        dataset = {
-            label: app_load_data['hourly'][wk]['trend_data_hourly']['words'][i],
-            fill: false,
-            lineTension: 0.3,
-            borderColor: colourlist[i],
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: colourlist[i],
-            pointHoverBorderColor: colourlist[i],
-            pointRadius: 1,
-            pointHitRadius: 15,
-            data: app_load_data['hourly'][wk]['trend_data_hourly']['value_list'][i]
-        }
-        hourlyTrendsChartData.datasets.push(dataset)
-    }
-    hourlyTrendsChart.update()
-}
-
-
 // CHANGE PARAMETERS
 
 function update_everything(start_selected, end_selected, top_seleced) {
 
   generate_data(start_selected, end_selected, parseInt(top_seleced))
+  console.log(top_seleced)
   //
   interactiveBarChart.data.labels = top_words_sorted
   interactiveBarChart.data.datasets[0].data = top_total_freqs_over_period_sorted
